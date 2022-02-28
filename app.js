7458db99d1fb881da4ae1efd2c068859c9d78316
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2022-02-28 20:57:22 +0300

    Fix: 404 error in erong roures

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ mongoose.connect("mongodb://localhost:27017/mestodb", (err) => {
 
 app.use(userRouter);
 app.use(cardsRouter);
+// если ни один из маршрутов не отвечает, то передаем ошибку 404
+app.use((req, res, next) => {
+  res.status(404).send({ message: "Ошибка 404 - Неправильный путь" });
+  next();
+});
 // запуск сервера, слушаем порт
 app.listen(PORT, () => {
   console.log("Run server...", PORT);
