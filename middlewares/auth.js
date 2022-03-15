198fc3f281740880a9243793e08a7bd16742ba35
Евгений Сергеев <akaevgeniy@yandex.ru>
2022-03-15 14:34:28 +0300

    Fix: auth unauth method

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,10 @@
 const jwt = require("jsonwebtoken");
+const Unauthorized = require("../errors/right-err");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .send({ message: "Необходима авторизация" });
+    throw new Unauthorized("Необходима авторизация");
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -13,9 +12,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, "some-secret-key");
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: "Необходима авторизация" });
+    next(err);
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
