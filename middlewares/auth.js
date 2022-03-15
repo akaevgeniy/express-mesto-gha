784dd1a6db25d7a20584ba03b70fc32f47c14b2e
Евгений Сергеев <akaevgeniy@yandex.ru>
2022-03-15 14:47:03 +0300

    Fix: auth middleware require

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const Unauthorized = require("../errors/right-err");
+const Unauthorized = require("../errors/unauth");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
