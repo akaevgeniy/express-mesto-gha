61e1e5824660564e4681f593f8b2ddbd750b4d7b
Евгений Сергеев <akaevgeniy@yandex.ru>
2022-03-15 15:08:18 +0300

    Fix: middlware remove

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ mongoose.connect("mongodb://localhost:27017/mestodb", (err) => {
 
 app.use(userRouter);
 app.use(cardsRouter);
-app.use(errors);
 // если ни один из маршрутов не отвечает, то передаем ошибку 404
 app.use((req, res, next) => {
   res.status(404).send({ message: "Ошибка 404 - Неправильный путь" });
   next();
 });
+app.use(errors);
 // запуск сервера, слушаем порт
 app.listen(PORT, () => {
   console.log("Run server...", PORT);
