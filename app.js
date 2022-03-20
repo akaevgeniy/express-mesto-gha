f4f96d8699a499960b643c5a71491ef3db991cf8
Евгений Сергеев <akaevgeniy@yandex.ru>
2022-03-20 21:23:51 +0300

    fix: 404 in app file

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const {
   createUser,
   login,
 } = require('./controllers/users');
+const NotFoundError = require('./errors/not-found-err');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -43,9 +44,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', (err) => {
 app.use(userRouter);
 app.use(cardsRouter);
 // если ни один из маршрутов не отвечает, то передаем ошибку 404
-app.use((req, res, next) => {
-  res.status(404).send({ message: 'Ошибка 404 - Неправильный путь' });
-  next();
+app.use(() => {
+  throw new NotFoundError('Ошибка 404 - Неправильный путь');
 });
 app.use(errors());
 app.use(errorsMiddleware);
